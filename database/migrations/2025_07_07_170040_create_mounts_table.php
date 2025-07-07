b8006c2824b00b37f917edc992684a781c1b53a5
Nathan <nathanhenrique64@gmail.com>
2025-07-07 14:32:12 -0300

    feat(database): definido schema e lógica para entidade Mount

--- database/migrations/2025_07_07_170040_create_mounts_table.php
+++ database/migrations/2025_07_07_170040_create_mounts_table.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('mounts', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('type');
+            $table->string('faction');
+            $table->string('xpac');
+            $table->string('source');
+            $table->string('image_url')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
+
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('mounts');
