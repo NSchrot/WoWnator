35a20d4a7cb2a774ec6f18f13c2c7327a5ee9565
Nathan <nathanhenrique64@gmail.com>
2025-07-07 14:29:20 -0300

    feat(database): definido schema e lógica para entidade Zone

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Zone extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'continent', 'image_url'];
 }
+
--- database/migrations/2025_07_07_170032_create_zones_table.php
+++ database/migrations/2025_07_07_170032_create_zones_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('zones', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('continent');
+            $table->string('image_url')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('zones');
