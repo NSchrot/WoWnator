9d3c6a43b274ee79a9b26bf80455b3308fe879e6
Nathan <nathanhenrique64@gmail.com>
2025-07-07 14:37:28 -0300

    feat(database): definido schema e lógica para entidade DailyChallenge

--- app/Models/DailyChallenge.php
+++ app/Models/DailyChallenge.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DailyChallenge extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['date', 'character_id', 'zone_id', 'quote_id', 'mount_id', 'skill_id'];
+
+    public function character() {
+        return $this->belongsTo(Character::class);
+    }
+
+    public function zone() {
+        return $this->belongsTo(Zone::class);
+    }
+
+    public function quote() {
+        return $this->belongsTo(Quote::class);
+    }
+
+    public function mount() {
+        return $this->belongsTo(Mount::class);
+    }
+
+    public function skill() {
+        return $this->belongsTo(Skill::class);
+    }
+    
 }
--- database/migrations/2025_07_07_165552_create_daily_challenges_table.php
+++ database/migrations/2025_07_07_165552_create_daily_challenges_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('daily_challenges', function (Blueprint $table) {
             $table->id();
+            $table->date('date')->unique();
+            $table->foreignId('character_id')->constrained('characters');
+            $table->foreignId('zone_id')->constrained('zones');
+            $table->foreignId('quote_id')->constrained('quotes');
+            $table->foreignId('mount_id')->constrained('mounts');
+            $table->foreignId('skill_id')->constrained('skills');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('daily_challenges');
