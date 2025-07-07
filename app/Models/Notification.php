d9b56c6391dd433c1c4ec68a76174ed0e2572c3d
Nathan <nathanhenrique64@gmail.com>
2025-07-07 14:42:47 -0300

    feat(database): definido schema e lógica para entidade Notification e executado migração inicial

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Notification extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'message',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'read_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2025_07_07_165603_create_notifications_table.php
+++ database/migrations/2025_07_07_165603_create_notifications_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->text('message');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
+
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('notifications');
