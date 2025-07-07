d847b1327d4616ffa1b380405a8ee9dfa8e7dce7
Nathan <nathanhenrique64@gmail.com>
2025-07-07 14:06:57 -0300

    feat(database): definido schema e lógica pra entidade Role

--- database/migrations/2025_07_07_165514_create_roles_table.php
+++ database/migrations/2025_07_07_165514_create_roles_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('roles');
